a041cd3a7e5421d605d27c148e54562d6e44e517
za006 <134183191+za006@users.noreply.github.com>
2026-05-31 00:32:45 +0800

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,1 +1,24 @@
+import { defineConfig } from "tinacms";
 
+export default defineConfig({
+  branch: "master", 
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "docs", 
+        fields: [
+          { type: "string", name: "title", label: "Title", isTitle: true, required: true },
+          { type: "rich-text", name: "body", label: "Body", isBody: true },
+        ],
+      },
+    ],
+  },
+});
